1d658c88891e78a8cd4484149f4333bad8e54cc1
rayansudeora <59887243+rayansudeora@users.noreply.github.com>
2020-06-04 13:16:26 -0700

    Added comments

--- drawgame.py
+++ drawgame.py
@@ -9,12 +9,13 @@ window_h = 700
 window = pygame.display.set_mode((window_w, window_h))#<--  #defining parameters for screen
 
 
+#defining boolean variables
 dark_drawing = False
 no_moving = False
 popping = False
 first_pause = False
 
-
+#title of game
 pygame.display.set_caption("Draw!")
 
 #defining some colors:
@@ -43,34 +44,21 @@ vel = 5
 
 
 
-font = pygame.font.SysFont('Arial', 70, bold=False, italic=False)
+font = pygame.font.SysFont('Arial', 70, bold=False, italic=False)#Our font (Arial, size 70)
 
 
-def message(msg, color, x, y):
+def message(msg, color, x, y): #function which will take the text, color, and position
 	screen_text = font.render(msg, True, color)
-	text_rect = screen_text.get_rect(center=(x, y))
+	text_rect = screen_text.get_rect(center=(x, y)) #rect for text
 	window.blit(screen_text, text_rect)
 
 message("PRESS 'p' TO", RED, window_w/2, ((window_h/2)-50))
 message("VIEW CONTROLS", RED, window_w/2, window_h/2)
 
-'''
-def pause():
-	paused = True
-	while paused:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				quit()
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_c: #if the user presses c, continue the drawing
-					paused = False
-'''
 
 font = pygame.font.SysFont('Arial', 30, bold=False, italic=False)
-def controls():
+def controls(): #display controls messages
 	message("CONTROLS: ", PINK, window_w/2, window_h-550)
-	pygame.display.update()
 	message("ARROW KEYS TO MOVE", GOLD, window_w/2, window_h-500)
 	message("'p' TO PAUSE, 'c' TO CONTINUE/UNDO", GOLD, window_w/2, window_h-450)
 	message("SPACE BAR TO ERASE/MOVE WITHOUT DRAWING", GOLD, window_w/2, window_h-400)
@@ -91,82 +79,79 @@ while run:
 
 
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
+		if event.type == pygame.QUIT: #if user exits, end the code
 			run = False
 	
-		if keys[pygame.K_p]:
-			if start:
+		if keys[pygame.K_p]: #if user presses p, pause the game
+			if start: #if this is the initial time the user pressed 'p'
 				first_pause = True
 				no_moving = True
-				window.fill(BLACK) 
+				window.fill(BLACK) #start the game
 				pygame.display.update()
 				new_window = window.copy()
-				controls()
-				start = False
+				controls() #show controls to user
+				start = False 
 			else:
-				no_moving = True
-				color_before_pause = color
-				color = BLACK
-				paused_x = x
-				paused_y = y
-				x = window_w - (window_w*2)
+				no_moving = True #if this is any time but the initial time the user pressed 'p'
+				color_before_pause = color #save the color
+				color = BLACK #hide color
+				paused_x = x #save x value
+				paused_y = y #save y value
+				x = window_w - (window_w*2) #take drawing square out of the screen
 				y = window_h - (window_h*2)
 				new_window = window.copy()
-				controls()
+				controls() #show controls to user
 					
 				
-		if keys[pygame.K_c]:
-			color = color_before_pause
-			window.blit(new_window, (0,0))
-			x = paused_x
+		if keys[pygame.K_c]: #is user presses c, continue the game
+			color = color_before_pause #make the drawing square the color it previously was
+			window.blit(new_window, (0,0)) #return the screen to what it looked like before pausing
+			x = paused_x #return x and y to where the drawing square was before pausing
 			y = paused_y
-			no_moving = False
+			no_moving = False #allow movement
 
 		if keys[pygame.K_1] or keys[pygame.K_KP1]:
 			color_before_pause = color
-			window.fill(BLACK)
+			window.fill(BLACK) #clear screen
 			pygame.display.update()
-			x = 30
+			x = 30 #reset position of drawing square
 			y = 30
-			no_moving = False
+			no_moving = False #allow movement
 			if first_pause:
-				first_pause = False
+				first_pause = False #allow user to choose a color
 			else:
-				color = color_before_pause
+				color = color_before_pause #make the drawing square the color it previously was
 			
 
 
-
-	#if keys[pygame.K_p]:
-	#	pause() #if user presses p, run the pause function
 	if keys[pygame.K_SPACE]:
-		dark_drawing = True
+		dark_drawing = True #allow user to move without drawing or erase
 		pos = []
-		pos.append(position)
+		pos.append(position) #keep list of movement so it can be erased
 		color = DARK
 
 
 
-	if keys[pygame.K_LEFT] and not no_moving and x>vel:
+	if keys[pygame.K_LEFT] and not no_moving and x>vel: #the user is allowed to move and must stay in the screen (boundary)
 		x -= vel #if the left arrow key is pressed, move left
 		position = (x,y)
 		if dark_drawing:
 			pos.append(position)
 			popping = True
 				
-	if keys[pygame.K_RIGHT] and not no_moving and x < window_w-width-vel:
+	if keys[pygame.K_RIGHT] and not no_moving and x < window_w-width-vel: #the user is allowed to move and must stay in the screen (boundary)
 		x += vel #if the right arrow key is pressed, move right
 		position = (x,y)
 		if dark_drawing:
 			pos.append(position)
 			popping = True
-	if keys[pygame.K_UP] and not no_moving and y>vel:
+	if keys[pygame.K_UP] and not no_moving and y>vel: #the user is allowed to move and must stay in the screen (boundary)
 		y -= vel #if the up arrow key is pressed, move up
 		position = (x,y)
 		if dark_drawing:
 			pos.append(position)
 			popping = True
-	if keys[pygame.K_DOWN] and not no_moving and y < window_h-height-vel:
+	if keys[pygame.K_DOWN] and not no_moving and y < window_h-height-vel: #the user is allowed to move and must stay in the screen (boundary)
 		y += vel #if the down arrow key is pressed, move down
 		position = (x,y)
 		if dark_drawing:
@@ -174,11 +159,11 @@ while run:
 			popping = True
 
 	if keys[pygame.K_r]:
-		dark_drawing = False
-		while popping:
+		dark_drawing = False #not erasing, so save all movement
+		while popping: #erase everywhere dark_drawing touched
 			pygame.draw.rect(window, BLACK, (pos[0][0], pos[0][1], width, height))
 			pos.pop(0)
-			if len(pos)==0:
+			if len(pos)==0:#once the list of all positions is empty, stop erasing
 				popping = False
 		color = RED #if user presses 'r', make the drawing color red
 			
@@ -238,7 +223,7 @@ while run:
 
 
 	
-	pygame.draw.rect(window, color, (x, y, width, height))
+	pygame.draw.rect(window, color, (x, y, width, height)) #draw rectangle
 	pygame.display.update()
 
 
