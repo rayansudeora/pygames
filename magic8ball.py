aeea17cfedae711a40a73695590510960c6ab29f
rayansudeora <59887243+rayansudeora@users.noreply.github.com>
2020-06-09 23:27:34 -0700

    Added comments, deleted unnecessary code

--- magic8ball.py
+++ magic8ball.py
@@ -1,6 +1,5 @@
 import pygame
 import random
-#import time
 import sys
 pygame.init()
 pygame.font.init()
@@ -8,7 +7,7 @@ keys = pygame.key.get_pressed()
 
 
 window_w = 700
-window_h = 750
+window_h = 750 
 
 window = pygame.display.set_mode((window_w, window_h))#<--  #defining parameters for screen
 pygame.display.update()
@@ -17,8 +16,9 @@ clear_window = window.copy()
 game_on = False
 after_attempt = False
 
-pygame.display.set_caption("Magic 8 Ball")
+pygame.display.set_caption("Magic 8 Ball") #title of game
 
+#defining colors with RGB format:
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 DARK = (20,20,20)
@@ -34,21 +34,21 @@ GOLD = (212, 175, 55)
 PINK = (255, 113, 181)
 
 
-font = pygame.font.SysFont('Arial', 53, bold=False, italic=False)#Our font (Arial, size 70)
+font = pygame.font.SysFont('Arial', 53, bold=False, italic=False)#Our font (Arial, size 53)
 
 def message(msg, color, x, y): #function which will take the text, color, and position
 	screen_text = font.render(msg, True, color)
 	text_rect = screen_text.get_rect(center=(x, y)) #rect for text
 	window.blit(screen_text, text_rect)
 
-def magicball():
+def magicball(): #main function which will randomly generate a response
 	num = random.randint(1,20)
 	if num==1:
-		response = pygame.image.load('magic8ballimages/as_i_see_it.png')
+		response = pygame.image.load('magic8ballimages/as_i_see_it.png')#1/20 chance each image will be portrayed
 		window.blit(response, (40,20))
 		pygame.display.update()
 	elif num==2:
-		response = pygame.image.load('magic8ballimages/ask_again_later.png')
+		response = pygame.image.load('magic8ballimages/ask_again_later.png')#each image is a different 8 ball response
 		window.blit(response, (40,20))
 		pygame.display.update()
 	elif num==3:
@@ -124,65 +124,33 @@ def magicball():
 		window.blit(response, (40,20))
 		pygame.display.update()
 
-def fade(width, height): 
-	fade = pygame.Surface((width, height))
-	fade.fill((0,0,0))
-	opacity = 0
-	for r in range(0, 300):
-		opacity += 1
-		fade.set_alpha(opacity)
-		window.fill(BLACK)
-		window.blit(fade, (0,0))
-		pygame.display.update()
-	pygame.time.delay(200)
-	for r in range(0, 300):
-		opacity -= 1
-		fade.set_alpha(opacity)
-		window.fill(BLACK)
-		window.blit(fade, (0,0))
-		pygame.display.update()
-
-
 message("MAGIC 8 BALL", MAROON, window_w/2, window_h/10)
-message("PRESS 1 TO BEGIN", GOLD, window_w/2, window_h/2)
-
-
-
-
+message("PRESS '1' TO BEGIN", GOLD, window_w/2, window_h/2)
 pygame.display.update()
 
-start = True
+start = True 
 run = True
 while run:
-	vel = 5
-	ball_x = 58
-	ball_y = 40
+
 	keys = pygame.key.get_pressed()
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: #if user exits, end the code
 			run = False
-		if keys[pygame.K_1] or keys[pygame.K_KP1] and start:
+		if keys[pygame.K_1] or keys[pygame.K_KP1] and start: #if user presses one, prepare the screen to ask another question
 			window.blit(clear_window, (0,0))
 			window.fill(LIGHT)
-			
-
 			user_q = ''
 			while user_q == '':
 				user_q = input("\n \n \nASK YOUR QUESTION: ")
-
 			ball = pygame.image.load('magic8ballimages/magic_8_ball.png')
-			window.blit(ball, (ball_x, ball_y))
-
-
-
+			window.blit(ball, (58, 40))
 
 			font = pygame.font.SysFont('Arial', 30, bold=False, italic=False)#Our font (Arial, size 70)
 			message("PRESS SPACE TO RECEIVE YOUR ANSWER", BLUE, window_w/2, window_h/14)
 			message("PRESS 1 TO ASK ANOTHER QUESTION!", PURPLE, window_w/2, window_h-50)
-			msg_window = window.copy()
 			pygame.display.update()
-			game_on = True
+			game_on = True 
 			start = False
 
 				
@@ -192,17 +160,15 @@ while run:
 			if event.type == pygame.QUIT:
 				run = False
 			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_SPACE:
+				if event.key == pygame.K_SPACE: 
 					print("ASKING 8 BALL: ")
 					dots = [".","..", "...", "....", ".....", "......", ".......", "........", ".........", ".........."]
-					for d in range(len(dots)):
+					for d in range(len(dots)): #for loop producing the loading dots while loading response
 					    pygame.time.delay(200)
 					    sys.stdout.write("\r" + dots[d % len(dots)])
 					    sys.stdout.flush()
 					print("\n")
-
-					window.blit(msg_window, (0,0))
-					magicball()
+					magicball() #run the main function
 					game_on = False
 
 
